9e7023137dcc64f98e3a2bc41bf1a0900ad7eb53
Webber <webber@takken.io>
2025-07-02 00:02:15 +0200

    chore: move out visual-migrator

--- visual-migrator/src/app/api/get-migration-data/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-import { getMigrationData } from './migration-service'
-
-export async function GET() {
-  const result = await getMigrationData()
-  return NextResponse.json(result, { status: result.success ? 200 : 500 })
-}
